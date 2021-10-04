e7eb687f0bca6c0e13098569387440a840ee070f
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-10-04 11:22:39 -0700

    fixed static route issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const htmlRoutes = require('./routes/htmlRoutes')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('./Develop/public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use('/api', apiRoutes);
