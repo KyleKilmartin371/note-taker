7bca49a72f26267fbed496b7c722dcb8a1cf499a
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-10-04 11:56:46 -0700

    fixed database post issues

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const { createNote,  deleteNote} = require('../../lib/notes');
-const { notes } = require('../../Develop/db/db');
+const { notes } = require('../../Develop/db/db.json');
 
 router.get('/notes', (req, res) => {
-    let savedNotes = notes;
-    res.json(savedNotes);
+    let saved = notes;
+    res.json(saved);
 })
+
 
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
